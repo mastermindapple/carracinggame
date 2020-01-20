6ffb6924ac41a934fb6a4dd592a9b2a074747dd0
Janav <dhingrajanavllc@gmail.com>
2020-01-20 19:53:42 +0530

    addiing of player form and game class

---
+++ js/Form.js
@@ -0,0 +1,27 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title = createElement('h2');
+        title.html("Car Racing Game");
+        title.position(130,0);
+        var input = createInput("Name");
+        input.position(130,160);
+        var button = createButton("Play");
+        button.position(250,200);
+        var greetings = createElement('h3');
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name = input.value();
+            playerCount += 1;
+            player.update(name);
+            player.updateCount(playerCount);
+            greetings.html("hello "+ name);
+            greetings.position(130,160);
+        })
+            
+        
+    }
+}
